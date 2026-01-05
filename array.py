5b30720826174c4f63b8354f77b9d68c5fe3a43d
Mohammed Raaid Ali <55349882+MohammedR8@users.noreply.github.com>
2026-01-05 12:09:26 +0300

    leetcode 121

--- array.py
+++ array.py
@@ -1,6 +1,6 @@
 #All the question are from the blind 75 list
 
-#Two Sum
+#1.Two Sum
 def twoSum(self, nums: List[int], target: int) -> List[int]:
     hsh =  {} #Using a hashmap
     diff = 0
@@ -13,3 +13,21 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         hsh[j] = i #We are just adding to the hashmap
     return lst #returning the list
+
+
+#121. Best time to buy and sell stock
+def maxProfit(self, prices: List[int]) -> int:
+    max_profit = 0
+    l = 0 #We are using two pointers left and right
+    r = 1
+
+    while r < len(prices): # loop over the list till we reach the end of the list, since r is ahead of l we use r here
+        if prices[l] < prices[r]: # check if the price we buy if less and the price we sell is more
+            profit = prices[r] - prices[l] # We calculate the profit 
+            max_profit = max(max_profit, profit) # Calculating max profit
+        else:
+            l = r # if prices[l] is more than prices[r] then just shift the pointer because goal is to buy at the lowest so we shift l where r was 
+        r += 1 # we just update r irrespective of any of the condition
+
+    return max_profit
+
